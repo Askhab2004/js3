910ef761026946c9f1453a592ace2701202bfa0a
Askhab2004 <123107421+Askhab2004@users.noreply.github.com>
2023-02-18 15:29:42 +0300

    js3
    js

---
+++ js Frontend/js/Index.js
@@ -0,0 +1,34 @@
+//Правильные переменные
+let a = 345
+let b = 785
+let c = 234
+let d = 342
+
+a = a + b;
+b = a - b;
+a = a - b;
+c = c + d;
+d = c - d
+c = c - d
+
+console.log(a,b,c,d)
+
+let j = 10
+console.log(j)
+console.log(j)
+console.log(j)
+
+
+//Неправильные переменные
+let la = 789
+
+console.log(la)
+
+
+
+//Askhab
+alert("Askhab")
+
+
+
+
